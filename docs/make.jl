e0392465db062be97cf7934720d6771062478e99
cormullion <cormullion@mac.com>
2017-07-16 16:52:05 +0100

    Update make.jl
    change repo

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(
   )
 
 deploydocs(
-    repo = "github.com/cormullion/ColorSchemes.jl.git",
+    repo = "github.com/JuliaGraphics/ColorSchemes.jl.git",
     target = "build",
     julia  = "0.6",
     osname = "osx",

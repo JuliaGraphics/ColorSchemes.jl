0a4003598ee926307cf6674061800c55a2770929
Nathan Daly <nhdaly@gmail.com>
2018-01-16 21:18:42 -0600

    Adds invert(cscheme,c), inverse of get(cscheme,x).
    `invert` places a color within a colorscheme, by converting the color to
    a value representing its position on the colorscheme's axis.
    
    Put another way, it returns the value in the domain for which its
    colorscheme value most closely matches the provided color.
    
    Also adds `convertToScheme(cscheme, img)`, which uses `invert` to
    convert every pixel in img to its closest color from the provided
    colorscheme.
    
    Adds tests for both methods.

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,6 +46,48 @@ function run_all_tests()
         @test startswith(lines[4], "RGB{Float64}(0.085")
     end
 
+    @testset "invert tests" begin
+        cs = linspace(RGB(0,0,0), RGB(1,1,1),5);
+        @test invert(cs, cs[3]) == 0.5
+
+        # Note that invert() takes the first closest match.
+        cs = [RGB(0,0,0), RGB(1,1,1),
+              RGB(0,0,0),
+              RGB(0,0,0), RGB(1,1,1)];
+        @test invert(cs, cs[3]) == 0
+
+        # Note that invert() takes the left index when two are identical.
+        cs = [RGB(1,1,1), RGB(0,0,0), RGB(0,0,0), RGB(0,1,1), RGB(1,1,0)];
+        @test invert(cs, cs[2]) == 0.25
+        cs = [RGB(0,0,0), RGB(0,0,0)];
+        @test invert(cs, cs[1]) == 0
+
+        cs = [RGB(0,0,0)];
+        @test_throws InexactError invert(cs, cs[1])
+        # (The above line throws for the same reason the below line does.
+        #  If this behavior ever changes, so should `invert`.)
+        @test_throws InexactError get(cs, 1.0, (1,1))
+    end
+
+
+    @testset "convertToScheme tests" begin
+        # Add color to a grayscale image.
+        red_cs = linspace(RGB(0,0,0), RGB(1,0,0),10)
+        gray_img = linspace(RGB(0,0,0), RGB(1,1,1),10)
+        vs = [invert(gray_img, p) for p in red_cs]
+        cs = [RGB(v,v,v) for v in vs]
+        rcs = [get(red_cs, p) for p in vs]
+        hcat(promote(red_cs,gray_img,cs,rcs)...)'
+        new_img = convertToScheme(red_cs, gray_img)
+        @test all(.≈(new_img, red_cs, atol=0.5))  # This is broken.. It should be way more specific. See next test.
+
+        # Should be able to uniquely match each increasing color with the next
+        # increasing color in the new scale.
+        red_cs = [linspace(RGB(0,0,0), RGB(1,0,0))...]
+        blue_scale_img = [linspace(RGB(0,0,0), RGB(0,0,1))...]
+        new_img = convertToScheme(red_cs, blue_scale_img)
+        @test_broken unique(new_img) == new_img
+    end
 end
 
 function run_minimum_tests()
@@ -61,7 +103,7 @@ function run_minimum_tests()
     # save as text
     colorscheme_to_text(hok, "hokusai_test_version", "hokusai_as_text.jl", comment="a test")
 
-    @test filesize("hokusai_as_text.jl") > 2000 
+    @test filesize("hokusai_as_text.jl") > 2000
 
     open("hokusai_as_text.jl") do f
         lines = readlines(f)

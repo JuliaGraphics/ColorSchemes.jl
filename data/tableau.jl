6b2b0e1efbb04b1ca1c9f61acc26c0fad1dae58a
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2020-09-18 09:42:13 +0200

    added palettes from Tableau

---
+++ data/tableau.jl
@@ -0,0 +1,200 @@
+loadcolorscheme(:tableau_10, [
+    colorant"#4E79A7", # blue
+    colorant"#F28E2B", # orange
+    colorant"#E15759", # red
+    colorant"#76B7B2", # turquoise
+    colorant"#59A14F", # green
+    colorant"#EDC948", # yellow
+    colorant"#B07AA1", # purple
+    colorant"#FF9DA7", # pink
+    colorant"#9C755F", # brown
+    colorant"#BAB0AC", # gray
+], "tableau")
+
+loadcolorscheme(:tableau_20, [
+    colorant"#4E79A7", colorant"#A0CBE8", # blue
+    colorant"#F28E2B", colorant"#FFBE7D", # orange
+    colorant"#59A14F", colorant"#8CD17D", # green
+    colorant"#B6992D", colorant"#F1CE63", # yellow
+    colorant"#499894", colorant"#86BCB6", # turquoise
+    colorant"#E15759", colorant"#FF9D9A", # red
+    colorant"#79706E", colorant"#BAB0AC", # gray
+    colorant"#D37295", colorant"#FABFD2", # pink
+    colorant"#B07AA1", colorant"#D4A6C8", # purple
+    colorant"#9D7660", colorant"#D7B5A6", # gray
+], "tableau")
+
+loadcolorscheme(:tableau_color_blind, [
+    colorant"#1170aa", 
+    colorant"#fc7d0b", 
+    colorant"#a3acb9",
+    colorant"#57606c", 
+    colorant"#5fa2ce",
+    colorant"#c85200",
+    colorant"#7b848f",
+    colorant"#a3cce9",
+    colorant"#ffbc79",
+    colorant"#c8d0d9",
+], "tableau")
+
+loadcolorscheme(:tableau_seattle_grays, [
+    colorant"#767f8b", 
+    colorant"#b3b7b8", 
+    colorant"#5c6068",
+    colorant"#d3d3d3", 
+    colorant"#989ca3",
+], "tableau")
+
+loadcolorscheme(:tableau_traffic, [
+    colorant"#b60a1c", 
+    colorant"#e39802", 
+    colorant"#309143", 
+    colorant"#e03531", 
+    colorant"#f0bd27", 
+    colorant"#51b364", 
+    colorant"#ff684c", 
+    colorant"#ffda66", 
+    colorant"#8ace7e", 
+], "tableau")
+
+loadcolorscheme(:tableau_miller_stone, [
+    colorant"#4f6980", 
+    colorant"#849db1", 
+    colorant"#a2ceaa", 
+    colorant"#638b66",
+    colorant"#bfbb60", 
+    colorant"#f47942", 
+    colorant"#fbb04e", 
+    colorant"#b66353", 
+    colorant"#d7ce9f", 
+    colorant"#b9aa97", 
+    colorant"#7e756d", 
+], "tableau")
+
+loadcolorscheme(:tableau_superfishel_stone, [
+    colorant"#6388b4",
+    colorant"#ffae34",
+    colorant"#ef6f6a",
+    colorant"#8cc2ca",
+    colorant"#55ad89",
+    colorant"#c3bc3f",
+    colorant"#bb7693",
+    colorant"#baa094",
+    colorant"#a9b5ae",
+    colorant"#767676",
+], "tableau")
+
+loadcolorscheme(:tableau_nuriel_stone, [
+    colorant"#8175aa",
+    colorant"#6fb899",
+    colorant"#31a1b3",
+    colorant"#ccb22b",
+    colorant"#a39fc9",
+    colorant"#94d0c0",
+    colorant"#959c9e",
+    colorant"#027b8e",
+    colorant"#9f8f12",
+], "tableau")
+
+loadcolorscheme(:tableau_jewel_bright, [
+    colorant"#eb1e2c",
+    colorant"#fd6f30",
+    colorant"#f9a729",
+    colorant"#f9d23c",
+    colorant"#5fbb68",
+    colorant"#64cdcc",
+    colorant"#91dcea",
+    colorant"#a4a4d5",
+    colorant"#bbc9e5",
+], "tableau")
+
+loadcolorscheme(:tableau_summer, [
+    colorant"#bfb202",
+    colorant"#b9ca5d",
+    colorant"#cf3e53",
+    colorant"#f1788d",
+    colorant"#00a2b3",
+    colorant"#97cfd0",
+    colorant"#f3a546",
+    colorant"#f7c480",
+], "tableau")
+
+loadcolorscheme(:tableau_winter,[
+    colorant"#90728f",
+    colorant"#b9a0b4",
+    colorant"#9d983d",
+    colorant"#cecb76",
+    colorant"#e15759",
+    colorant"#ff9888",
+    colorant"#6b6b6b",
+    colorant"#bab2ae",
+    colorant"#aa8780",
+    colorant"#dab6af",
+], "tableau")
+
+loadcolorscheme(:tableau_green_orange_teal, [
+    colorant"#4e9f50",
+    colorant"#87d180",
+    colorant"#ef8a0c",
+    colorant"#fcc66d",
+    colorant"#3ca8bc",
+    colorant"#98d9e4",
+    colorant"#94a323",
+    colorant"#c3ce3d",
+    colorant"#a08400",
+    colorant"#f7d42a",
+    colorant"#26897e",
+    colorant"#8dbfa8",
+], "tableau")
+
+loadcolorscheme(:tableau_red_blue_brown, [
+    colorant"#466f9d",
+    colorant"#91b3d7",
+    colorant"#ed444a",
+    colorant"#feb5a2",
+    colorant"#9d7660",
+    colorant"#d7b5a6",
+    colorant"#3896c4",
+    colorant"#a0d4ee",
+    colorant"#ba7e45",
+    colorant"#39b87f",
+    colorant"#c8133b",
+    colorant"#ea8783",
+], "tableau")
+
+loadcolorscheme(:tableau_purple_pink_gray, [
+    colorant"#8074a8",
+    colorant"#c6c1f0",
+    colorant"#c46487",
+    colorant"#ffbed1",
+    colorant"#9c9290",
+    colorant"#c5bfbe",
+    colorant"#9b93c9",
+    colorant"#ddb5d5",
+    colorant"#7c7270",
+    colorant"#f498b6",
+    colorant"#b173a0",
+    colorant"#c799bc",
+], "tableau")
+
+loadcolorscheme(:tableau_hue_circle, [
+    colorant"#1ba3c6",
+    colorant"#2cb5c0",
+    colorant"#30bcad",
+    colorant"#21B087",
+    colorant"#33a65c",
+    colorant"#57a337",
+    colorant"#a2b627",
+    colorant"#d5bb21",
+    colorant"#f8b620",
+    colorant"#f89217",
+    colorant"#f06719",
+    colorant"#e03426",
+    colorant"#f64971",
+    colorant"#fc719e",
+    colorant"#eb73b3",
+    colorant"#ce69be",
+    colorant"#a26dc2",
+    colorant"#7873c0",
+    colorant"#4f7cba",
+], "tableau")

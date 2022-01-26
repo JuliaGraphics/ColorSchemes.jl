a9dddb3a7e16d25c5d57171dcb6ebba38540d19c
Davi Barreira <davisbarreira@gmail.com>
2022-01-25 22:11:23 -0300

    :zap: New colorschemes based on Julia colors.

---
+++ data/jcolors.jl
@@ -0,0 +1,101 @@
+loadcolorscheme(:julia, [
+    colorant"#1f83ff", # blue
+    colorant"#CA3C32", # red
+    colorant"#399746", # green
+    colorant"#9259A3", # purple
+], "julia", "sequential")
+
+loadcolorscheme(:juliaextended, [
+    colorant"#1f83ff", # blue
+    colorant"#CA3C32", # red
+    colorant"#399746", # green
+    colorant"#9259A3", # purple
+    colorant"#FFAE00", # yellow
+    colorant"#9ca3af", # gray
+], "julia", "sequential")
+
+loadcolorscheme(:jblue, [
+    colorant"#d2e6ff";,
+    colorant"#a5cdff";,
+    colorant"#79b5ff";,
+    colorant"#4c9cff";,
+    colorant"#1f83ff";,
+    colorant"#1969cc";,
+    colorant"#134f99";,
+    colorant"#0c3466";,
+    colorant"#061a33";,
+], "julia", "sequential")
+
+loadcolorscheme(:jred, [
+    colorant"#f4d8d6";,
+    colorant"#eab1ad";,
+    colorant"#df8a84";,
+    colorant"#d5635b";,
+    colorant"#ca3c32";,
+    colorant"#a23028";,
+    colorant"#79241e";,
+    colorant"#511814";,
+    colorant"#280c0a";,
+], "julia", "sequential")
+
+loadcolorscheme(:jgreen, [
+    colorant"#d7eada";,
+    colorant"#b0d5b5";,
+    colorant"#88c190";,
+    colorant"#61ac6b";,
+    colorant"#399746";,
+    colorant"#2e7938";,
+    colorant"#225b2a";,
+    colorant"#173c1c";,
+    colorant"#0b1e0e";,
+], "julia", "sequential")
+
+loadcolorscheme(:jredgreen, [
+    colorant"#ca3c32";,
+    colorant"#d5635b";,
+    colorant"#df8a84";,
+    colorant"#eab1ad";,
+    colorant"#f4d8d6";,
+    colorant"#f9fafb";,
+    colorant"#d7eada";,
+    colorant"#b0d5b5";,
+    colorant"#88c190";,
+    colorant"#61ac6b";,
+    colorant"#399746";,
+], "julia", "diverging")
+
+loadcolorscheme(:jpurple, [
+    colorant"#e9deed";,
+    colorant"#d3bdda";,
+    colorant"#be9bc8";,
+    colorant"#a87ab5";,
+    colorant"#9259a3";,
+    colorant"#754782";,
+    colorant"#583562";,
+    colorant"#3a2441";,
+    colorant"#1d1221";,
+], "julia", "sequential")
+
+loadcolorscheme(:jyellow, [
+    colorant"#ffefcc";,
+    colorant"#ffdf99";,
+    colorant"#ffce66";,
+    colorant"#ffbe33";,
+    colorant"#ffae00";,
+    colorant"#cc8b00";,
+    colorant"#996800";,
+    colorant"#664600";,
+    colorant"#332300";,
+], "julia", "sequential")
+
+loadcolorscheme(:jgray, [
+    colorant"#f3f4f6";,
+    colorant"#e5e7eb";,
+    colorant"#d1d5db";,
+    colorant"#9ca3af";,
+    colorant"#6b7280";,
+    colorant"#4b5563";,
+    colorant"#374151";,
+    colorant"#1f2937";,
+    colorant"#111827";,
+], "julia", "sequential")
